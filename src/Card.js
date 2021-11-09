16ec0e0e7dbd1a1f975cd09a8d034bfca5f536db
Omicron <rsamal.b305@gmail.com>
2021-11-09 14:47:57 +0530

    Used function to create card template

--- src/Card.js
+++ src/Card.js
@@ -1,51 +1,44 @@
 import styles from './card.module.css'
 import Tilt from 'react-parallax-tilt';
-
 import Scroll from './scrollbar'
-function Card(){
-return( 
-<>
-<Scroll/>
-<div className={styles.container} >
-<Tilt className={styles.box}  gyroscope={true} tiltReverse={true}>
-<div  >
-    <span></span>
-    <div className = {styles.content}>
-        <h2>Pranav K R</h2>
-        <p>Name: Pranav K R<br/>
-            Srn: PES2UG20CS247<br/>
-            Sec: 'D' </p>
-        <a href="https://github.com/ProBrother7">Read More</a>
-    </div>
-</div>
-</Tilt>
-<Tilt className={styles.box}  gyroscope={true} tiltReverse={true}>
-<div  >
-    <span></span>
-    <div className = {styles.content}>
-        <h2>Prajay V K</h2>
-        <p>Name: Prajay V K<br/>
-            Srn: PES2UG20CS245<br/>
-            Sec: 'D' </p>
-        <a href="https://github.com/PrajayVK">Read More</a>
-    </div>
-</div>
-</Tilt>
-<Tilt className = {styles.box}  gyroscope={true} tiltReverse={true}>
-<div  >
-    <span></span>
-    <div className = {styles.content}>
-        <h2>Rahul Samal</h2>
-        <p>Name: Rahul Samal<br/>
-           Srn: PES2UG20CS262<br/>
-           Sec: 'D'
-            </p>
-        <a href="https://github.com/Omicron02">Read More</a>
-    </div>
-</div>
-</Tilt>
-</div>
 
-</>
-);}
+function Card()
+{
+    const cardData =
+    [
+        { name: "Pranav K R", srn: "247", github: "ProBrother7" },
+        { name: "Prajay VK", srn: "245", github: "PrajayVK" },
+        { name: "Rahul Samal", srn: "262", github: "Omicron02" }
+    ]
+
+    const cardMap = cardData.map(element => 
+        <CardData key = {element.name} name = {element.name} srn = {element.srn} github = {element.github}/>)
+
+    return( 
+    <>
+        <Scroll/>
+        <div className={styles.container}>
+            {cardMap}
+        </div>
+    </>
+    )
+}
+
+function CardData(props)
+{
+    return(
+    <Tilt className={styles.box} gyroscope={true} tiltReverse={true}>
+        <span/>
+        <div className = {styles.content}>
+            <h2>{props.name}</h2>
+            <p>
+                Name: {props.name}<br/>
+                Srn: PES2UG20CS{props.srn}<br/>
+                Sec: 'D' 
+            </p>
+            <a href = {"https://github.com/" + props.github} target = "_blank">Read More</a>
+        </div>
+    </Tilt>
+    )
+}
 export default Card;
