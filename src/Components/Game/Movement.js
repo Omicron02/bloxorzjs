7429ee03849c28eed68c780074f1142013e50a2e
ProBrother7 <pranavkr2002@gmail.com>
2021-12-06 21:02:46 +0530

    Fixed movement

--- src/Components/Game/Movement.js
+++ src/Components/Game/Movement.js
@@ -46,7 +46,7 @@ function DropTest(orient, ref, grid, api)
         var p1 = [Math.round(ref.current.position.x/10), Math.round((ref.current.position.z-5)/10)]
         var p2 = [Math.round(ref.current.position.x/10), Math.round((ref.current.position.z+5)/10)]
         console.log(p1, p2)
-        if (p1[0] < 0 || p1[0] >=grid[0].length || p1[1] < 0 || p2[1] > grid.length || grid[p1[1]][p1[0]] === "0" || grid[p2[1]][p2[0]] === "0")
+        if (p1[0] < 0 || p1[0] >=grid[0].length || p1[1] < 0 || p2[1] >=grid.length || grid[p1[1]][p1[0]] === "0" || grid[p2[1]][p2[0]] === "0")
         {
             api.start({pos: [ref.current.position.x, 
                 ref.current.position.y-50,
@@ -61,7 +61,7 @@ function DropTest(orient, ref, grid, api)
         var p1 = [Math.round((ref.current.position.x-5)/10), Math.round(ref.current.position.z/10)]
         var p2 = [Math.round((ref.current.position.x+5)/10), Math.round(ref.current.position.z/10)]
         console.log(p1, p2)
-        if (p1[2] < 0 || p1[1] >=grid.length || p1[0] < 0 || p2[0] > grid.length || grid[p1[1]][p1[0]] === "0" || grid[p2[1]][p2[0]] === "0")
+        if (p1[1] < 0 || p1[1] >= grid.length || p1[0] < 0 || p2[0] >= grid[0].length || grid[p1[1]][p1[0]] === "0" || grid[p2[1]][p2[0]] === "0")
         {
             api.start({pos: [ref.current.position.x, 
                 ref.current.position.y-50,
