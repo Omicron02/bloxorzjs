f3308cc8def2fd73b111b1d507facbc04e835dc6
Omicron <rsamal.b305@gmail.com>
2021-12-06 20:30:27 +0530

    redirect to login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //  import Styles from './App.module.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {useState} from "react"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import Firstpage from "./Components/Firstpage/Firstpage.js"
 import Card from "./Components/Card/Card.js"
 import Signup from "./Components/Signup/Signup.js"
 import Leaderboard from "./Components/Leaderboard/leaderboard.js"
 import Instruction from "./Components/Instructions/Instructions.js"
+import Game from "./Components/Game/Game.js"
+
 function App()
 {
+    const [user, setUser] = useState("")
     return (
         <Router>
             <Routes>
@@ -14,7 +18,9 @@ function App()
 
                 <Route path = "/credits" element = {<Card/>}/>
 
-                <Route path = "/login" element = {<Signup/>}/>
+                <Route path = "/game" element = {user?<Game/>:<Navigate to = "/login" />}/>
+
+                <Route path = "/login" element = {<Signup setUser = {setUser}/>}/>
 
                 <Route path = "/instruction" element = {<Instruction/>}/>
 
